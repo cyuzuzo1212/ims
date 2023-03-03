a773c2e3697b690b4f6dec5ac26ca2ca52cf7e9d
cyuzuzo1212 <119512993+cyuzuzo1212@users.noreply.github.com>
2023-03-03 17:43:39 +0200

    addition of some modifications on homepage

--- src/components/Landingpage/home.js
+++ src/components/Landingpage/home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "../../App.css";
+import img from "../../images/aboutimage.jpg";
+import { TiSocialFacebook, TiSocialLinkedin, TiSocialTwitter,TiSocialUtube} from "react-icons/ti";
 
 function Home() {
   const workInfoData = [
@@ -41,6 +43,21 @@ function Home() {
           <button className="learn">Learn More</button>
         </div>
       </div>
+
+
+<div className="about-section" style={{marginBottom: "300px"}}>
+<h1 style={{margin:"80px 0px 0px 60px"}}>About Us</h1>
+    <div className="about-text">
+       
+        <h2 style={{color:"blue",paddingBottom:"50PX"}}>Welcome to our Inventory Management system</h2>
+        <p >Our system is designed to help businesses improve efficiency.</p>
+            <p>With our user-friendly interface and powerful features,</p>
+            <p>you can easily track inventory levels, generate reports and analytics.</p>  
+    </div>
+    <img src={img} alt="about" className="right"></img>
+</div>
+
+
       <div className="work-section-wrapper" style={{ marginBottom: "300px" }}>
         <div className="work-section-top">
           <p className="primary-subheading">Services</p>
@@ -60,7 +77,10 @@ function Home() {
               <p>{data.text}</p>
             </div>
           ))}
-        </div>
+
+      </div>
+        <div className="contact-background">
+          <div className="white-ground">
         <div className="contact-container">
           <div className="contact-left">
             <h2 className="contact-text1">Contact Us</h2>
@@ -79,21 +99,21 @@ function Home() {
 
           <div className="contact-right">
             <form className="contact-form">
-              <input
+              <input style={{padding:"10px 0px 10px 30px"}}
                 type="text"
                 placeholder="Your name"
                 name="user_name"
                 required
               />
 
-              <input
+              <input style={{padding:"10px 20px 10px",marginLeft:"26px"}}
                 type="text"
                 placeholder="Your Email"
                 name="user_email"
                 required
               />
-              <div className="namemail">
-                <input
+              <div  >
+                <input style={{margin:"20px 0px 20px 0px", padding:"10px 135px 10px"}}
                   type="text"
                   placeholder="Subject"
                   name="subject"
@@ -101,12 +121,56 @@ function Home() {
                 />
               </div>
 
-              <textarea name="message" cols="50" rows="10"></textarea>
-              <button type="submit" className="send-sms">
-                Send Message
-              </button>
+              <textarea name="message" cols="60" rows="7"></textarea>
+              <button type="submit" className="send-sms" style={{
+              marginLeft:"200px",
+              marginTop:"10px",
+              padding:"10px",
+              border:"none",
+              borderRadius:"10px",
+              backgroundColor:"blue",
+              color:"white"}}> Send Message </button>
             </form>
           </div>
+        </div>
+        </div>
+        </div>
+
+        <div className="footer">
+          <div className="sb-footer section-padding">
+            <div className="sb-footer-links">
+              <div className="sb-footer-links-div">
+                <h4>For Business</h4>
+                <a href="employer">
+                <p>employer</p></a>
+                <a href="plan">
+                <p>plan</p></a>
+
+                <a href="individual">
+                <p>individual</p></a>
+              </div>
+              <div className="sb-footer-links-div">
+              <h4>resourses</h4>
+              <a href="employer">
+                <p>employer</p></a>
+                <a href="plan">
+                <p>plan</p></a>
+
+                <a href="individual">
+                <p>individual</p></a>
+            </div>
+            <div className="sb-footer-links-div">
+              <h4>resourses</h4>
+              <a href="employer">
+                <p>employer</p></a>
+                <a href="plan">
+                <p>plan</p></a>
+
+                <a href="individual">
+                <p>individual</p></a>
+                </div>
+                
+          </div></div>
         </div>
       </div>
     </div>
